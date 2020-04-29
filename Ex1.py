4a74e38a1aa9f4ad22ce5ebfd009f1ee4e0f29f9
Lu Cheng <summabonas@gmail.com>
2020-04-29 22:17:52 +0200

    add some annotations

--- Ex1.py
+++ Ex1.py
@@ -7,18 +7,18 @@ from mpl_toolkits.mplot3d import Axes3D
 mu, sigma = 1, 0.2  # mean and standard deviation
 sample = 1000
 bins = 30
-e_gaussian = np.random.normal(mu, sigma, sample)
+e_gaussian = np.sort(np.random.normal(mu, sigma, sample))
 
-fig1 = plt.figure()
+fig1 = plt.figure(figsize=(40, 20))
 # the experiment
 ax1 = fig1.add_subplot(121)
 count, x, ignored = ax1.hist(e_gaussian, bins, density=True)
-g_gaussian = 1/(sigma * np.sqrt(2 * np.pi)) * np.exp(-(x - mu)**2 / (2 * sigma**2))
+g_gaussian = 1/(sigma * np.sqrt(2 * np.pi)) * np.exp(-(e_gaussian - mu)**2 / (2 * sigma**2))
 
 # the ground truth
 ax2 = fig1.add_subplot(122)
-ax2.plot(x, g_gaussian, color='r')
-# show
+ax2.plot(e_gaussian, g_gaussian, color='r')
+# show result
 plt.show()
 
 
